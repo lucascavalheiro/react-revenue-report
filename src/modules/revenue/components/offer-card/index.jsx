7ec2e7a29b56254b9e00f5d206211a62d7cf4530
Lucas Cavalheiro <lucasad.cavalheiro@gmail.com>
2020-10-16 02:06:36 -0300

    add balance sheet

--- src/modules/revenue/components/offer-card/index.jsx
+++ src/modules/revenue/components/offer-card/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Card from '../../../components/card';
-import { selectOfferDescription } from '../../store/revenue.store';
 import './offer-card.scss';
 import OfferDescription from './offer-description';
 import OfferName from './offer-name';
 
 const OfferCard = () => {
-  const description = useSelector(selectOfferDescription);
-
   return (
     <Card>
       <div className='offer-card-content'>
--- src/modules/revenue/components/revenue-page/index.jsx
+++ src/modules/revenue/components/revenue-page/index.jsx
@@ -3,8 +3,10 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import DropdownDatePicker from '../../../components/dropdown-date-picker';
 import useFetchRevenue from '../../data/use-fetch-revenue';
+import RevenueBalanceSheet from '../revenue-balance-sheet';
 import OfferCard from '../offer-card';
 import './revenue-page.scss';
+import OfferBalanceSheet from '../offer-balance-sheet';
 
 const RevenuePage = () => {
   const [date, setDate] = useState(null);
@@ -23,6 +25,10 @@ const RevenuePage = () => {
           wrapperClassName='date-picker-full-width'
           dateFormat='MMMM dd, yyyy'
         />
+
+        <RevenueBalanceSheet />
+
+        <OfferBalanceSheet />
       </div>
     </div>
   );
