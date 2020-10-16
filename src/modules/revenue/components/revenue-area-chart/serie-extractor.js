56af9288ae54334159a68c84cf35e42eb1aaab63
Lucas Cavalheiro <lucasad.cavalheiro@gmail.com>
2020-10-16 08:03:15 -0300

    fix typo at serie data extractor

--- src/modules/revenue/components/revenue-area-chart/serie-extractor.js
+++ src/modules/revenue/components/revenue-area-chart/serie-extractor.js
@@ -2,14 +2,14 @@ import { format } from 'date-fns';
 
 export const extractSerieData = (validations = []) => {
   const dateHash = validations.reduce((prev, curr) => {
-    const { amount, createAt, offer, total } = curr;
+    const { amount, createdAt, offer, total } = curr;
 
     if (!offer) return prev;
 
     const gross = Math.round(amount * offer.unitCost * 100) / 100;
     const net = Math.round((gross - total) * 100) / 100;
 
-    const key = format(new Date(createAt), 'HH a').toLowerCase();
+    const key = format(new Date(createdAt), 'HH a').toLowerCase();
     if (prev[key]) {
       prev[key] = Math.round((prev[key] + net) * 100) / 100;
     } else {
