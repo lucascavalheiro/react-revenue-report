b8dc1cf172a7242f6822f395034bf03f9b47750e
Lucas Cavalheiro <lucasad.cavalheiro@gmail.com>
2020-10-16 08:05:35 -0300

    add error message to date picker

--- src/modules/components/dropdown-date-picker/index.jsx
+++ src/modules/components/dropdown-date-picker/index.jsx
@@ -3,14 +3,20 @@ import './dropdown-date-picker.scss';
 
 class DropdownDatePicker extends React.Component {
   render() {
-    const { value, onClick } = this.props;
+    const { value, onClick, error } = this.props;
+    const classes = ['ddl-date-picker'];
+    if (error) classes.push('error');
+
     return (
-      <div className='ddl-date-picker' onClick={onClick}>
-        <span>{value || 'Select a date...'}</span>
-        <div>
-          <span>&#9660;</span>
+      <>
+        <div className={classes.join(' ')} onClick={onClick}>
+          <span>{value || 'Select a date...'}</span>
+          <div>
+            <span>&#9660;</span>
+          </div>
         </div>
-      </div>
+        {error && <div className='error-text'>{error}</div>}
+      </>
     );
   }
 }
