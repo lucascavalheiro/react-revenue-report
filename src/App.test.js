278d352ed5d55d576d7d6f5c1a9818ae42dccb1d
Lucas Cavalheiro <lucasad.cavalheiro@gmail.com>
2020-10-23 09:25:06 -0300

    remove inital test failing

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-test('renders learn react link', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('Revenue').name()).toEqual('Revenue');
-});
